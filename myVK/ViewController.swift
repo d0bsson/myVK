75b4753408cd86f44f1caaec906cefe9cfa207d1
David <ber.dev@yandex.ru>
2022-01-03 19:21:15 +0300

    Initial Commit

---
+++ myVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myVK
+//
+//  Created by Дэвид Бердников on 03.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
