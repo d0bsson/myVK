f55a758f04e00fcd088a7ed3a8843baebce12260
David <ber.dev@yandex.ru>
2022-01-08 22:28:00 +0300

    cells is work

--- myVK/Newsfeed/NewsfeedViewController.swift
+++ myVK/Newsfeed/NewsfeedViewController.swift
@@ -9,55 +9,51 @@
 import UIKit
 
 protocol NewsfeedDisplayLogic: AnyObject {
-  func displayData(viewModel: Newsfeed.Model.ViewModel.ViewModelData)
+    func displayData(viewModel: Newsfeed.Model.ViewModel.ViewModelData)
 }
 
 class NewsfeedViewController: UIViewController, NewsfeedDisplayLogic {
-
-  var interactor: NewsfeedBusinessLogic?
-  var router: (NSObjectProtocol & NewsfeedRoutingLogic)?
-
+    
+    var interactor: NewsfeedBusinessLogic?
+    var router: (NSObjectProtocol & NewsfeedRoutingLogic)?
+    
     @IBOutlet weak var table: UITableView!
     
-  
-  // MARK: Setup
-  
-  private func setup() {
-    let viewController        = self
-    let interactor            = NewsfeedInteractor()
-    let presenter             = NewsfeedPresenter()
-    let router                = NewsfeedRouter()
-    viewController.interactor = interactor
-    viewController.router     = router
-    interactor.presenter      = presenter
-    presenter.viewController  = viewController
-    router.viewController     = viewController
-  }
-  
-  // MARK: Routing
-  
-
-  
-  // MARK: View lifecycle
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-      setup()
-      
-      table.register(UINib(nibName: "NewsfeedCell", bundle: nil), forCellReuseIdentifier: NewsfeedCell.reuseId)
-  }
-  
-  func displayData(viewModel: Newsfeed.Model.ViewModel.ViewModelData) {
-      
-      switch viewModel {
-      case .some:
-          print(".some ViewController")
-      case .displayNewsFeed:
-          print(".DisplayNewsFeed ViewController")
-      }
-      
-  }
-  
+    // MARK: Setup
+    
+    private func setup() {
+        let viewController        = self
+        let interactor            = NewsfeedInteractor()
+        let presenter             = NewsfeedPresenter()
+        let router                = NewsfeedRouter()
+        viewController.interactor = interactor
+        viewController.router     = router
+        interactor.presenter      = presenter
+        presenter.viewController  = viewController
+        router.viewController     = viewController
+    }
+    
+    // MARK: Routing
+    
+    
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        
+        table.register(UINib(nibName: "NewsfeedCell", bundle: nil), forCellReuseIdentifier: NewsfeedCell.reuseId)
+    }
+    
+    func displayData(viewModel: Newsfeed.Model.ViewModel.ViewModelData) {
+        switch viewModel {
+        case .some:
+            print(".some ViewController")
+        case .displayNewsFeed:
+            print(".DisplayNewsFeed ViewController")
+        }
+    }
 }
 
 extension NewsfeedViewController: UITableViewDelegate, UITableViewDataSource {
